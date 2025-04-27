93f2a66f90ad7b88bf7ce1659cb0766c453382f0
Denys Smirnov <dennwc@pm.me>
2025-04-27 13:19:10 +0300

    Support iter.Seq.

--- testit/testit.go
+++ testit/testit.go
@@ -9,37 +9,32 @@ import (
 	"github.com/dennwc/iters"
 )
 
+func checkRes[T any](t testing.TB, exp, got []T, expErr, gotErr error) {
+	t.Helper()
+	if expErr != nil {
+		must.Eq(t, expErr, gotErr)
+	} else {
+		must.NoError(t, gotErr)
+	}
+	must.Eq(t, exp, got)
+}
+
 func ExpectIter[T any](t testing.TB, exp []T, expErr error, it iters.Iter[T]) {
 	defer it.Close()
 	got, err := iters.All(it)
-	if expErr != nil {
-		must.Eq(t, expErr, err)
-	} else {
-		must.NoError(t, err)
-	}
-	must.Eq(t, exp, got)
+	checkRes(t, exp, got, expErr, err)
 }
 
 func ExpectIterCtx[T any](t testing.TB, exp []T, expErr error, it iters.IterCtx[T]) {
 	ctx := context.Background()
 	defer it.Close()
 	got, err := iters.AllCtx(ctx, it)
-	if expErr != nil {
-		must.Eq(t, expErr, err)
-	} else {
-		must.NoError(t, err)
-	}
-	must.Eq(t, exp, got)
+	checkRes(t, exp, got, expErr, err)
 }
 
 func ExpectPageIter[T any](t testing.TB, exp []T, expErr error, it iters.PageIter[T]) {
 	ctx := context.Background()
 	defer it.Close()
 	got, err := iters.AllPages(ctx, it)
-	if expErr != nil {
-		must.Eq(t, expErr, err)
-	} else {
-		must.NoError(t, err)
-	}
-	must.Eq(t, exp, got)
+	checkRes(t, exp, got, expErr, err)
 }
